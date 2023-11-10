1e49a4f273627776d26869f800f1285e9f3f31da
bounassif <rabihbounassif@hotmail.com>
2023-11-10 17:43:59 -0500

    Initial Commit

---
+++ Adenoma Detector/Adenoma_DetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adenoma_DetectorApp.swift
+//  Adenoma Detector
+//
+//  Created by Bou Nassif, Rabih/Sloan Kettering Institute on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Adenoma_DetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Adenoma Detector/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Adenoma Detector
+//
+//  Created by Bou Nassif, Rabih/Sloan Kettering Institute on 11/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
